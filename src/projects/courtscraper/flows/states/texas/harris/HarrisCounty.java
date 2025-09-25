22cf3f8a41c6faa73052e8bda0bc9faa28446ca2
Saifullah Khan <saif4rmkhan@gmail.com>
2025-09-25 15:21:09 +0500

    fix file not downloading issue

--- src/projects/courtscraper/flows/states/texas/harris/HarrisCounty.java
+++ src/projects/courtscraper/flows/states/texas/harris/HarrisCounty.java
@@ -4,6 +4,7 @@ import courtscraper.datamanagement.json.JSONGrabbers;
 import courtscraper.flows.states.StateSelect;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -12,60 +13,83 @@ import java.time.Duration;
 
 import static courtscraper.helpers.TabManagement.switchTab;
 
-
 public class HarrisCounty extends StateSelect {
 
-    private static WebDriverWait wait;
+  private static WebDriverWait wait;
 
-    public static void harrisMain(String caseNumber) throws FileNotFoundException, InterruptedException {
-        wait = new WebDriverWait(driver, Duration.ofSeconds(60));
-        //grabs harris site and logs in only if reset has recently happened (aka. Google webpage)
-        if (driver.getCurrentUrl().equals("https://www.google.com/")) {
-            loginHarris();
-        }
-
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='txtCaseNumber']"))).sendKeys(caseNumber);
-
-        Thread.sleep(6000);
-
-        try {
-            while (!driver.findElement(By.className("captcha-solver")).getAttribute("data-state").equals("solved")) {
-                Thread.sleep(1);
-            }
-        } catch (NoSuchElementException e) {
-            System.out.println("no captcha");
-        }
-
-        Thread.sleep(500);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#ctl00_ctl00_ctl00_ContentPlaceHolder1_ContentPlaceHolder2_ContentPlaceHolder2_btnCivSearch"))).click();
-        Thread.sleep(1500);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@title='View Case Details']"))).click();
-        Thread.sleep(2000);
-
-        switchTab(1);
-
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"tabDocuments\"]"))).click();
-
-        new HarrisDocketRetrieval().retrieveDockets(); //starts harris retrieval flow
-
-        driver.close();
-        switchTab(0);
-
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"ctl00_ctl00_ctl00_ContentPlaceHolder1_ContentPlaceHolder2_ContentPlaceHolder2_btnSearchAgain\"]"))).click();
-
-        renameFilesBulk(); //renames case files
+  public static void harrisMain(String caseNumber)
+      throws FileNotFoundException, InterruptedException {
+    wait = new WebDriverWait(driver, Duration.ofSeconds(60));
+    // grabs harris site and logs in only if reset has recently happened (aka. Google webpage)
+    if (driver.getCurrentUrl().equals("https://www.google.com/")) {
+      loginHarris();
     }
 
-    private static void loginHarris() throws InterruptedException, FileNotFoundException {
-        driver.get("https://www.hcdistrictclerk.com/eDocs/Public/Search.aspx");
-        String[] credentials = new JSONGrabbers().loginGrabber("TexasHarris");
+    wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='txtCaseNumber']")))
+        .sendKeys(caseNumber);
 
-        Thread.sleep(500);
-        driver.findElement(By.xpath("//*[@id='txtUserName']")).sendKeys(credentials[0]);
-        Thread.sleep(500);
-        driver.findElement(By.xpath("//*[@id='txtPassword']")).sendKeys(credentials[1]);
-        Thread.sleep(500);
-        driver.findElement(By.xpath("//*[@id='btnLogin']")).click();
-        Thread.sleep(500);
+    Thread.sleep(6000);
+
+    try {
+      while (!driver
+          .findElement(By.className("captcha-solver"))
+          .getAttribute("data-state")
+          .equals("solved")) {
+        Thread.sleep(1);
+      }
+    } catch (NoSuchElementException e) {
+      System.out.println("no captcha");
     }
+
+    Thread.sleep(500);
+    wait.until(
+            ExpectedConditions.visibilityOfElementLocated(
+                By.cssSelector(
+                    "#ctl00_ctl00_ctl00_ContentPlaceHolder1_ContentPlaceHolder2_ContentPlaceHolder2_btnCivSearch")))
+        .click();
+    Thread.sleep(1500);
+    wait.until(
+            ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//a[@title='View Case Details']")))
+        .click();
+    Thread.sleep(2000);
+
+    switchTab(1);
+
+    wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"tabDocuments\"]")))
+        .click();
+
+    new HarrisDocketRetrieval().retrieveDockets(); // starts harris retrieval flow
+
+    driver.close();
+    switchTab(0);
+
+    wait.until(
+            ExpectedConditions.visibilityOfElementLocated(
+                By.xpath(
+                    "//*[@id=\"ctl00_ctl00_ctl00_ContentPlaceHolder1_ContentPlaceHolder2_ContentPlaceHolder2_btnSearchAgain\"]")))
+        .click();
+
+    renameFilesBulk(); // renames case files
+  }
+
+  private static void loginHarris() throws InterruptedException, FileNotFoundException {
+    driver.get("https://www.hcdistrictclerk.com/eDocs/Public/Search.aspx");
+    String[] credentials = new JSONGrabbers().loginGrabber("TexasHarris");
+
+    // Create WebDriverWait (10 seconds timeout)
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+    // Wait until the element is clickable (or visible)
+    WebElement userNameInput =
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='txtUserName']")));
+
+    // Now send keys
+    userNameInput.sendKeys(credentials[0]);
+    Thread.sleep(500);
+    driver.findElement(By.xpath("//*[@id='txtPassword']")).sendKeys(credentials[1]);
+    Thread.sleep(500);
+    driver.findElement(By.xpath("//*[@id='btnLogin']")).click();
+    Thread.sleep(500);
+  }
 }
--- src/projects/courtscraper/flows/states/texas/harris/HarrisDocketRetrieval.java
+++ src/projects/courtscraper/flows/states/texas/harris/HarrisDocketRetrieval.java
@@ -3,7 +3,10 @@ package courtscraper.flows.states.texas.harris;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.List;
 
 import static courtscraper.helpers.CheckIfRetrieved.fileDownloadedCheck;
@@ -14,7 +17,14 @@ public class HarrisDocketRetrieval extends HarrisCounty {
 
     public void retrieveDockets() throws InterruptedException {
 
-        List<WebElement> rows = driver.findElements(By.xpath("/html/body/form/div[3]/div/div/div/div[3]/div[15]/div/section/div/div/div[3]/table/tbody/tr"));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+    // Wait until at least one row is present in the table
+        List<WebElement> rows =
+            wait.until(
+                ExpectedConditions.presenceOfAllElementsLocatedBy(
+                    By.xpath(
+                        "/html/body/form/div[3]/div/div/div/div[3]/div[15]/div/section/div/div/div[3]/table/tbody/tr")));
         int count = 0;
         //defines link for compatibility with the try catch for grabbing the link below
         WebElement link;
@@ -23,12 +33,18 @@ public class HarrisDocketRetrieval extends HarrisCounty {
             if (element.getAttribute("outerHTML").contains(">Order<") || count <= 5) {
                 //finds anchor for the link to click and download
                 try {
-                    link = element.findElement(By.xpath(".//a"));
-                } catch (Exception e) {
+              // Wait until the <a> inside this element is visible
+                  link =
+                      wait.until(
+                              ExpectedConditions.visibilityOfNestedElementsLocatedBy(
+                                  element, By.xpath(".//a")))
+                          .get(0);
+        }
+                catch (Exception e) {
                     continue;
                 }
 
-                docketNames.add(element.findElement(By.xpath("./td/table/tbody/tr/td[1]")).getAttribute("innerHTML").replace("\n", " ").replace(",", "").replace("\u00A0", "").replace("/", " ").replace("\"", " ").trim()); //grabs name of document for renaming purposes
+                docketNames.add(element.findElement(By.xpath("./td[3]")).getAttribute("innerText").replace("\n", " ").replace(",", "").replace("\u00A0", "").replace("/", " ").replace("\"", " ").trim()); //grabs name of document for renaming purposes
                 docketNumbers.add(link.getText()); //grabs docket number for renaming and puts them on a list for later use
 
                 ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", link); //scrolls to the link to ensure it is clicked properly
